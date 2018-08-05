f3aa80c654b25e478242b55d90ab7f26ab5d4c65
Neil Rosenstech <nrosenstech@liaison-intl.com>
2018-08-05 14:14:50 -0400

    Module5 complete

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,13 +10,23 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 };
   }
 
+  showNextQuestion() {
+    this.setState((prevState) => {
+      return { quiz_position: prevState.quiz_position + 1 };
+    });
+  }
+
   render() {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
 
     return (
     <div>
       { isQuizEnd && <QuizEnd /> }
-      { !isQuizEnd && <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/> }
+      {
+        !isQuizEnd && <QuizQuestion
+        showNextQuestionHandler={this.showNextQuestion.bind(this)}
+        quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+      }
     </div>
     );
   }
